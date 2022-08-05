75ba3ccdb53cc946ecbfb06913da4e3baf447bd6
¨Vinícius <vini.ceriotti@hotmail.com>
2022-08-04 21:55:08 -0300

    Database seeding

--- backend/src/main/java/com/ceriotti/catalog/services/CategoryService.java
+++ backend/src/main/java/com/ceriotti/catalog/services/CategoryService.java
@@ -6,17 +6,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ceriotti.catalog.entities.Category;
 import com.ceriotti.catalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
+
+	@Transactional(readOnly = true)
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
 
