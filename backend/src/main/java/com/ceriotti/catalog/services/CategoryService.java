8cc5801d9bc7b305bdafc7514184fbc3349db4aa
¨Vinícius <vini.ceriotti@hotmail.com>
2022-08-04 22:14:15 -0300

    DTO

--- backend/src/main/java/com/ceriotti/catalog/services/CategoryService.java
+++ backend/src/main/java/com/ceriotti/catalog/services/CategoryService.java
@@ -3,11 +3,13 @@
 package com.ceriotti.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ceriotti.catalog.dto.CategoryDTO;
 import com.ceriotti.catalog.entities.Category;
 import com.ceriotti.catalog.repositories.CategoryRepository;
 
@@ -18,8 +20,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
