f0f1775d4799320dc59dad5faff973233b6e93c3
¨Vinícius <vini.ceriotti@hotmail.com>
2022-08-06 00:15:22 -0300

    Pagination

--- backend/src/main/java/com/ceriotti/catalog/services/CategoryService.java
+++ backend/src/main/java/com/ceriotti/catalog/services/CategoryService.java
@@ -2,7 +2,6 @@
 
 package com.ceriotti.catalog.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -11,6 +10,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,9 +28,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x)); // Page já é um stream()
 	}
 
 	@Transactional(readOnly = true)
